7a3904cda0ddf33820eea2408d359eefa9451750
kwan641 <rattanapong.c@gmail.com>
2026-03-25 09:18:01 +0700

    Auto Deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1075626118"
+    serviceWorkerVersion: "2764028845"
   }
 });
